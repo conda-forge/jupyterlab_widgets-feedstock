e12a23a7b881d357a682307f4958a95f591e8d25
Nicholas Bollweg <nick.bollweg@gmail.com>
2026-02-04 18:16:30 -0600

    more assets

--- recipe/copy_assets.py
+++ recipe/copy_assets.py
@@ -1,13 +1,21 @@
 from pathlib import Path
 import shutil
-import sys
 import os
+import sys
 
-SHARE = "share/jupyter/labextensions/@jupyterlab-widgets/jupyterlab-manager"
-SRC = Path(os.environ["BUILD_PREFIX"]) / SHARE
-DEST = Path(sys.prefix) / SHARE
+SHARE = os.environ["JS_ASSETS"]
+BUILD_SHARE = Path(os.environ["BUILD_PREFIX"]) / SHARE
+PREFIX_SHARE = Path(os.environ["PREFIX"]) / SHARE
+SRC_DIR = Path(os.environ["SRC_DIR"])
+
+
+def main() -> int:
+    PREFIX_SHARE.parent.mkdir(parents=True)
+    shutil.copytree(BUILD_SHARE, PREFIX_SHARE)
+    third_party = PREFIX_SHARE / "static/third-party-licenses.json"
+    shutil.copy2(third_party, SRC_DIR / third_party.name)
+    return 0
 
 
 if __name__ == "__main__":
-    DEST.parent.mkdir(parents=True)
-    shutil.copytree(SRC, DEST)
+    sys.exit(main())
